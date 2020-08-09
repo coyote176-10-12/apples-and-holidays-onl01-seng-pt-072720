73f8b2f9a6cd9cfc12f053972ae02c0b615c4f50
Javier Aguilar <coyote176@msn.com>
2020-08-09 15:04:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,10 +50,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-   holiday = final_holiday.join(" ")
-
-      supply = supply.join(", ")
-      puts "  #{holiday}: #{supply}"
+   
   #holiday_hash.each do |seasons, holidays|
     #puts "#{seasons}: #{holidays}"
   # iterate through holiday_hash and print items such that your readout resembles:
